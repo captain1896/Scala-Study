def6a782887dcaef1cbeeb1cd4814a71288d5f0c
lingdongrushui <lingdongrushui@gmail.com>
2013-07-30 18:19:32 +0800

    add more matching pattern test case

--- src/com/lucas/study/scala/CaseClassAndPatternMatching/TestCaseClassAndMatchingPattern.scala
+++ src/com/lucas/study/scala/CaseClassAndPatternMatching/TestCaseClassAndMatchingPattern.scala
@@ -16,6 +16,9 @@ object TestCaseClassAndMatchingPattern {
     testValMatchingPattern
     testMath
     testConstructorPattern
+    testSerializeMatchingPattern
+    testTupleMatchingPattern
+    testTypedPattern
   }
 
   def testCaseClass() {
@@ -91,5 +94,53 @@ object TestCaseClassAndMatchingPattern {
     println(constructorPattern(BinOp("+",Var("Y"),Number(9.0))))
   }
 
+  def serializeMatchingPattern(x:Any) = x match {
+    case List(0,_,_) => println("serializeMatchingPattern Found It!")
+    case _ =>  println("serializeMatchingPattern Not Match!")
+  }
 
+  /**
+   *匹配任意長度的序列，可以指定_*作為模式的最後元素。
+   * 這種古怪的模式能匹配序列中零到任意數量的元素。
+   * 下面的方法匹配由零開始，不計長度的任意列表的模式。
+   * @param x
+   */
+  def serializeMatchingPattern2(x:Any) = x match {
+    case List(0,_*) => println("serializeMatchingPattern2 Found It!")
+    case _ =>  println("serializeMatchingPattern2 Not Match!")
+  }
+
+  def testSerializeMatchingPattern() {
+    println(serializeMatchingPattern(Nil))
+    println(serializeMatchingPattern(List(1,2,3)))
+    println(serializeMatchingPattern(List(0,1,4,5)))
+    println(serializeMatchingPattern(List(0,1)))
+    println(serializeMatchingPattern(List(0,1,4)))
+    serializeMatchingPattern2(List(0,1,4))
+    println("---Unit---:" + serializeMatchingPattern2(List(0,1,4)))
+  }
+
+  def tupleDemo(x:Any) = x match {
+    case (a,b,c) => println("matched :" + a + b + c)
+    case _ => println("tupleDemo Not Match !")
+  }
+
+  def testTupleMatchingPattern() {
+    tupleDemo(("ENGLAND|","SCOTLAND|","IRELAND"))
+    tupleDemo(("","","",""))
+    tupleDemo("")
+
+  }
+
+  def typedPattern(x:Any) = x match {
+    case s:String => println("Matched!"); s.length
+    case m:Map[_,_] => println("Matched!"); m.size
+    case _ => 1
+  }
+
+  def testTypedPattern() = {
+    println(typedPattern("Maven"))
+    println(typedPattern(Map(1 -> 'a' , 2 -> 'b')))
+  }
 }
+
