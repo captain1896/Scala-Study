3b75988683c6ec0ba0183c8b2f9d104a7cd4fb65
lingdongrushui <lingdongrushui@gmail.com>
2013-07-31 19:10:41 +0800

    add scala option type test

--- src/com/lucas/study/scala/CaseClassAndPatternMatching/Option/TestOption.scala
+++ src/com/lucas/study/scala/CaseClassAndPatternMatching/Option/TestOption.scala
@@ -15,6 +15,8 @@ object TestOption {
   def main(args: Array[String]) {
     testOption
     useMatchingPattern
+    testOptionUsage
+    testOptionUsage2
   }
 
   /**
@@ -25,23 +27,78 @@ object TestOption {
    */
   def testOption() {
     val capitals = Map[String, String]("France" -> "Paris", "Japan" -> "Tokyo")
-    println(capitals.get("France"))  //Some(x)
-    println(capitals get "China")    //None
+    println(capitals.get("France")) //Some(x)
+    println(capitals get "China") //None
 
     println(show(capitals.get("Japan")))
     println(show(capitals.get("France")))
     println(show(capitals.get("China")))
     println(show(capitals.get("North Pole")))
   }
+
   //分離可選值最通常的辦法是通過模式匹配
-  def show(x:Option[String]) = x match {
+  def show(x: Option[String]) = x match {
     case Some(s) => s
     case None => "?"
   }
 
   def useMatchingPattern() {
-    val myTuple = (123,"abc")
-    val (number,string) = myTuple
+    val myTuple = (123, "abc")
+    val (number, string) = myTuple
     println("number=" + number + "|" + "string=" + string)
   }
+
+  def testOptionUsage() {
+    val footballPlayerMap: Map[String, String] = Map[String, String]("Zidane" -> "France", "David Beckham" -> "England")
+    val zidan = footballPlayerMap.get("Zidane") //some
+    val beckham = footballPlayerMap.get("David Beckham") //some
+    val kaka = footballPlayerMap.get("kaka") //none
+    println("zidan.isDefined:" + zidan.isDefined + "|zidan.isEmpty:" + zidan.isEmpty + "|zidan.get:" + zidan.get + "!")
+    println(beckham)
+    println("beckham.isDefined:" + beckham.isDefined + "|beckham.isEmpty:" + beckham.isEmpty + "|beckham.get:" + beckham.get + "!")
+    println(kaka)
+    println("kaka.isDefined:" + kaka.isDefined + "|kaka.isEmpty:" + kaka.isEmpty + "|kaka.get!")
+
+  }
+
+  def testOptionUsage2() {
+    val testMap: Map[String, String] = Map("Nintama Rantarou" -> "忍者亂太郎", "Touch" -> "棒球小子", "Slam Dunk" -> "男兒當入樽")
+    val nintama: Option[String] = testMap.get("Nintama Rantarou")
+    val touch: Option[String] = testMap.get("Bingo")
+    val slamDunk: Option[String] = testMap.get("Slam Dunk")
+
+    nintama match {
+      case Some(name) => println("Nintama Rantarou:" + name)
+      case None => println("Do Nothing")
+    }
+
+    touch match {
+      case Some(name) => println("Touch:" + name)
+      case None => println("棒球小子，Sorry, It's None!")
+    }
+
+    slamDunk match {
+      case Some(name) => println("Slam Dunk:" + name)
+      case None => println("Sorry,Slam Dunk isEmpty!")
+    }
+
+    for (nin <- nintama) {
+      println("nin.length=" + nin.length)
+    }
+
+    for (tou <- touch) {
+      println("tou.legth=" + tou.length)
+    }
+
+    val vx = List("Hello,", "Goodbye,", "Oh,My ")
+    val vxNew = vx.map(_ + "World!")
+    for (v <- vxNew) println(v)
+    vxNew.foreach(println)
+
+    vx.map("|Lucas " + _).map(_ + ".length=").foreach(println)
+    vx.map("length=" + _.length).foreach(println)
+
+  }
+
+
 }
