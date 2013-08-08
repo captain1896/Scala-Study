1fc128486bcbc6594cf5d21e30e1c63a3a5e39da
lingdongrushui <lingdongrushui@gmail.com>
2013-08-08 19:19:16 +0800

    add scala list implementing test case

--- src/com/lucas/study/scala/ListUsage/ImplementingLists/ListTest.scala
+++ src/com/lucas/study/scala/ListUsage/ImplementingLists/ListTest.scala
@@ -2,6 +2,7 @@ package com.lucas.study.scala.ListUsage.ImplementingLists
 
 import java.lang.Integer
 import java.lang.System
+import scala.collection.mutable.ListBuffer
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,6 +15,9 @@ object ListTest {
   def main(args: Array[String]) {
     test
     testList
+    testIncAll
+    testIncAll2
+    testListBuffer
   }
 
   //類型參數前的+號特指列表是協變的，因為這一屬性。比如，List[Int]類型可以誒類型為List[Any]的變量
@@ -40,13 +44,13 @@ object ListTest {
    * 這三種方法在List中都是抽象的，它們被定義在子對象Nil和子類::中。
    * List的層級結構
    *
-   *         List[+T]
-   *    <<sealed abstract>>
-   *           \
-   *     ----------------
-   *     \               \
-   *     ::               Nil
-   *  <<final case>>   <<case object>>
+   * List[+T]
+   * <<sealed abstract>>
+   * \
+   * ----------------
+   * \               \
+   * ::               Nil
+   * <<final case>>   <<case object>>
    *
    */
 
@@ -62,24 +66,63 @@ object ListTest {
     val emptyList = List()
     //nil.head        //拋出NoSuchElementException異常
     //nil.tail        //拋出NoSuchElementException異常
-    nil.isEmpty       //true
+    nil.isEmpty //true
 
     //emptyList.head  //拋出NoSuchElementException異常
     //emptyList.tail  //拋出NoSuchElementException異常
-    emptyList.isEmpty  //true
+    emptyList.isEmpty //true
 
-    val numbers = List("ONE","TWO","THREE","FOUR")
-    val newNumbers = "ZERO"::numbers
+    val numbers = List("ONE", "TWO", "THREE", "FOUR")
+    val newNumbers = "ZERO" :: numbers
     println(newNumbers)
 
-    val vcs1 = List("Git","BitKeeper","Monotone")
-    val vcs2 = List("SVN","VSS")
+    val vcs1 = List("Git", "BitKeeper", "Monotone")
+    val vcs2 = List("SVN", "VSS")
     val vcs = vcs1 ::: vcs2
     println(vcs)
 
+  }
+
+  def incAll(xs: List[Int]): List[Int] = xs match {
+    case List() => List()
+    case x :: xs1 => x + 1 :: incAll(xs1)
+  }
+
+  def testIncAll() {
+    val numbers = List(1, 2, 3, 4, 5)
+    val numbersInc = incAll(numbers)
+    println("numbers   =>" + numbers)
+    println("numbersInc=>" + numbersInc)
+  }
+
+  def incAll2(xs: List[Int]): List[Int] = {
+    var result: List[Int] = Nil
+    for (x <- xs)
+      result = result ::: List(x + 1)
+    result
+  }
+
+  def testIncAll2() {
+    val numbers = List(11, 12, 13, 14)
+    val numbersNew = incAll2(numbers)
+    println(numbers)
+    println(numbersNew)
+  }
 
 
+  def testListBuffer() {
+    val buf = new ListBuffer[Int]
+    buf += 1
+    buf += 2
+    buf += 3
+    println(buf)
+    println("buf.toList=" + buf.toList)
 
+    val forBuf = new ListBuffer[Int]
+    for (x <- buf) forBuf += x + 1
+    println("---buf ListBuffer----")
+    println(forBuf)
+    println(forBuf.toList)
   }
 
 }
